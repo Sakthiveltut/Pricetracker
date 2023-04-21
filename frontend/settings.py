2a9421571b7c62ba1b6f8218bd0ae96b7dd6db32
Sakthiveltut <msakthiveltut@gmail.com>
2023-04-22 00:11:31 +0530

    admin page setup 5

--- frontend/settings.py
+++ frontend/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
-    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -51,10 +50,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
+
 ]
 
-CORS_ALLOWED_ORIGINS = [    'https://price-tracker.up.railway.app',]
+
 
 ROOT_URLCONF = 'frontend.urls'
 
