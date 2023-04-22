d431075c3db2d8b584db5af8b4c84f676ce1e0fa
Sakthiveltut <msakthiveltut@gmail.com>
2023-04-23 00:59:28 +0530

    textfiled

--- app/models.py
+++ app/models.py
@@ -20,7 +20,7 @@ class Category(models.Model):
 
 class FProducts(models.Model):
     category=models.ForeignKey(Category,on_delete=models.CASCADE)
-    name=models.CharField(max_length=1000)
+    name=models.TextField(max_length=1000)
     image_link=models.CharField(max_length=500)
     url=models.CharField(max_length=1000)
     offer=models.CharField(max_length=100)
